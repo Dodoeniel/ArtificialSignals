01638df366815662f28605cb307ed392130405ac
Dodoeniel <daniel.schoepflin@ tuhh.de>
2018-10-15 11:04:31 +0200

    test

--- PythonCode/helperfunctions.py
+++ PythonCode/helperfunctions.py
@@ -5,11 +5,12 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Flatten
+from keras.layers import Dropout
 
 DELIMITER = ','
 
 
-def read_file_names(filename,path):
+def read_file_names(filename, path):
     """ read_FileNames(filename,path) returns the List of names of time series files based on the given path and name
     of the file where the names are stored. """
     namesList = []
@@ -20,7 +21,7 @@ def read_file_names(filename,path):
     return namesList
 
 
-def read_labels(filename,path):
+def read_labels(filename, path):
     """ read_Labels(filename,path) returns the List of Labels of time series files based on the given path and name
         of the file where the labels are stored. """
     labelList = []
@@ -31,55 +32,67 @@ def read_labels(filename,path):
     return labelList
 
 
-def read_one_time_series(filename,path):
+def read_one_time_series(filename, path):
     """ read_one_TimeSeries(filename,path) returns the matrix of the time series data, specified by filename and path.
     The file is specified as """
-    data = np.loadtxt(path+filename,delimiter=DELIMITER)
+    data = np.loadtxt(path+filename, delimiter=DELIMITER)
     return data
 
 
-def shape_data(name_list,path):
+def shape_data_all(name_list, path): # for all datasets at once
     """ [samples,timesteps,features]"""
     array3d = []
+    array2d = []
     for i in range(len(name_list)):
-        array2d = np.array(read_one_time_series(name_list[i][0],path))
+        array2d = np.array(read_one_time_series(name_list[i][0], path))
         array3d.append(array2d)
     array3d = np.array(array3d)
-    array3d.reshape(len(name_list),array2d.shape[0],array2d.shape[1])
+    array3d.reshape(len(name_list), array2d.shape[0], array2d.shape[1])
     return array3d
 
 
+def shape_data_one(name, path):  # if only one dataset shall be used e.g. for loop training --> output 2 dimensional
+    """ [samples,timesteps,features]"""
+    array2d = np.array(read_one_time_series(name, path))
+    np.reshape(array2d, array2d.shape + (1,))
+    return array2d
+
+
 def define_model(training_data):
-    model = Sequential()
-    lstm_size_l1 = 500                # size of the hidden layer 1
-    model.add(LSTM(lstm_size_l1, input_shape=(training_data.shape[1],training_data.shape[2]), return_sequences=True))
+    m = Sequential()
+    lstm_size_l1 = 13                 # size of the hidden layer 1
+    m.add(LSTM(lstm_size_l1, input_shape=(training_data.shape[1], training_data.shape[2]), return_sequences=True,
+               recurrent_dropout=0.2))
+    m.add(Dropout(0.2))
     # input shape: numTimeSteps, numFeatures per TimeStep
-    model.add(LSTM(lstm_size_l1,return_sequences=True))
-    model.add(Flatten())
+    m.add(LSTM(2*lstm_size_l1,return_sequences=True, recurrent_dropout=0.2))
+    m.add(Dropout(0.2))
+    m.add(LSTM(lstm_size_l1,return_sequences=True, recurrent_dropout=0.2))
+    m.add(Dropout(0.2))
+    m.add(Flatten())
     # output layer for classification
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])  # for classification problems
+    m.add(Dense(1, activation='sigmoid'))
+    m.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])  # for classification problems
+    plot(m, to_file='model.png', show_shapes=True, show_layer_names=True)
+    return m
 
-    return model
 
-
-if __name__== '__main___':
+if __name__ == '__main___':
     path = 'TestRun1/'
     fileNameNames = 'NamesTraining.csv'
     fileNameLabels = 'LabelsTraining.csv'
 
-    c = read_file_names(fileNameNames,path)
-    x = shape_data(c,path)
-    y = np.array(read_labels(fileNameLabels,path))
+    c = read_file_names(fileNameNames, path)
+    x = shape_data_all(c, path)
+    y = np.array(read_labels(fileNameLabels, path))
     model = define_model(x)
     model.fit(x, y, epochs=1, batch_size=1)
 
     fileNameNames = 'NamesTest.csv'
     fileNameLabels = 'LabelsTest.csv'
-    c = read_file_names(fileNameNames,path)
-    x = shape_data(c,path)
-    y = np.array(read_labels(fileNameLabels,path))
-    score = model.evaluate(x,y,batch_size=1)
+    c = read_file_names(fileNameNames, path)
+    x = shape_data_all(c, path)
+    y = np.array(read_labels(fileNameLabels, path))
+    score = model.evaluate(x, y, batch_size=1)
     print(score)
     print('Hallo')
-
--- PythonCode/mainfunction.py
+++ PythonCode/mainfunction.py
@@ -1,25 +1,22 @@
 import helperfunctions as hf
+import numpy as np
 
 PATH = 'TestRun1/'
 
-trainingNames = hf.read_file_names('NamesTraining', PATH)
-trainingLabels = hf.read_labels('LabelsTraining',PATH)
+trainingNames = hf.read_file_names('NamesTraining.csv', PATH)
+trainingLabels = hf.read_labels('LabelsTraining.csv', PATH)
 
 # network definition based on one training set
-model = hf.define_model(hf.shape_data(trainingNames[0], PATH))
+model = hf.define_model(hf.shape_data_all(trainingNames, PATH))
 
-# train network in loop over all training sets
-for i in range(len(trainingNames)):
-    x = hf.shape_data(trainingNames[i], PATH)
-    y = trainingLabels[i]
-    model.fit(x, y, epochs=10, batch_size=10)
+x = hf.shape_data_all(trainingNames, PATH)
+y = np.array(trainingLabels)
+model.fit(x, y, epochs=10, batch_size=20, validation_split=0.33)
 
 
-fileNameNames = 'NamesTest.csv'
-fileNameLabels = 'LabelsTest.csv'
-c = hf.read_file_names(fileNameNames, PATH)
-x = hf.shape_data(c, PATH)
-y = np.array(hf.read_labels(fileNameLabels, PATH))
-score = model.evaluate(x, y, batch_size=1)
+testNames = hf.read_file_names('NamesTest.csv', PATH)
+x = hf.shape_data_all(testNames, PATH)
+y = np.array(hf.read_labels('LabelsTest.csv', PATH))
+score = model.evaluate(x, y, batch_size=5)
 print(score)
 
